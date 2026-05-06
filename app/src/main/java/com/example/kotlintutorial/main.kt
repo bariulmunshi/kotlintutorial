e85792bb1bed1d52eea712d5c6e8ae7290508312
bariulmunshi <bariulsoft@gmail.com>
2026-05-06 23:59:42 +0600

    first commit

---
+++ app/src/main/java/com/example/kotlintutorial/main.kt
@@ -0,0 +1,14 @@
+package com.example.kotlintutorial
+// 1
+fun main(){
+    println("Hello World")
+    var name:String = "Bariul"
+    name= "Rakib"
+    val age:Int= 25
+
+    println(name)
+    println(age)
+
+}
+
+//2
