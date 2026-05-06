8cbe0d92908ad5d70d493a0ab4a16a90184d1d4a
bariulmunshi <bariulsoft@gmail.com>
2026-05-07 00:29:57 +0600

    conditonal added

--- app/src/main/java/com/example/kotlintutorial/main.kt
+++ app/src/main/java/com/example/kotlintutorial/main.kt
@@ -21,3 +21,28 @@ package com.example.kotlintutorial
 }
 
 */
+
+// variable & datatype
+/*fun main(){
+    val name: String = "Rahat" // নাম পাল্টাবে না
+    var age: Int = 20          // বয়স পাল্টাতে পারে
+    val gpa: Double = 3.85     // দশমিক সংখ্যা
+    val isStudent: Boolean = true // হ্যাঁ অথবা না
+    println(name)
+    println(age)
+    println(gpa)
+    println(isStudent)
+}*/
+
+//Conditional If/Else
+/*fun main(){
+    val marks = 70
+
+    if (marks >= 80) {
+        println("You have gotten A+!")
+    } else {
+        println("You have to do more good")
+    }
+}*/
+
+
